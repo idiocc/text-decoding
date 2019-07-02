e9d5867e27adaf1ae002bf213307c6aed4bf7044
Anton <anton@adc.sh>
2019-07-02 13:47:54 +0300

    b

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 This is a polyfill for the [Encoding Living Standard](https://encoding.spec.whatwg.org/) API for the Web, allowing encoding and decoding of textual data to and from Typed Array buffers for binary data in JavaScript.
 
-By default it adheres to the spec and does not support encoding to legacy encodings, only decoding. It is also implemented to match the specification's algorithms, rather than for performance. The intended use is within Web pages, so it has no dependency on server frameworks or particular module schemes.
+By default it adheres to the spec and does not support encoding to legacy encodings, only decoding. It is also implemented to match the specification's algorithms, rather than for performance.
 
 ```sh
 yarn add text-decoding
--- build/index.js
+++ build/index.js
@@ -1,34 +1,16 @@
-const { debuglog } = require('util');
+const TextEncoder = require('./lib/TextEncoder');
+const TextDecoder = require('./lib/TextDecoder');
+const EncodingIndexes = require('./encoding-indexes');
+const { getEncoding } = require('./lib');
 
-const LOG = debuglog('text-decoding')
-
-/**
- * [fork] Polyfill for the Encoding Living Standard's API Written In ES6 And Optimised With JavaScript Compiler.
- * @param {_text-decoding.Config} [config] Options for the program.
- * @param {boolean} [config.shouldRun=true] A boolean option. Default `true`.
- * @param {string} config.text A text to return.
- */
-               async function textDecoding(config = {}) {
-  const {
-    shouldRun = true,
-    text,
-  } = config
-  if (!shouldRun) return
-  LOG('text-decoding called with %s', text)
-  return text
-}
-
-/* documentary types/index.xml */
-/**
- * @suppress {nonStandardJsDocs}
- * @typedef {_text-decoding.Config} Config Options for the program.
- */
-/**
- * @suppress {nonStandardJsDocs}
- * @typedef {Object} _text-decoding.Config Options for the program.
- * @prop {boolean} [shouldRun=true] A boolean option. Default `true`.
- * @prop {string} text A text to return.
- */
+//
+// Implementation of Encoding specification
+// https://encoding.spec.whatwg.org/
+//
 
 
-module.exports = textDecoding
+
+module.exports.TextEncoder = TextEncoder
+module.exports.TextDecoder = TextDecoder
+module.exports.EncodingIndexes = EncodingIndexes
+module.exports.getEncoding = getEncoding
